017ccdadd855b31138fa10ec74fe272c0acb790a
Simone Sada <simonesada@3caravelle.com>
2025-10-08 15:39:25 +0200

    fix: update utils script to rename dist files to solve build issues

--- utils.js
+++ utils.js
@@ -1,3 +1,22 @@
-var fs = require('fs'); 
-fs.createReadStream('./dist/codice.fiscale.var.js')
-.pipe(fs.createWriteStream('./dist/codice-fiscale-var.js'))
+const fs = require('fs');
+const path = require('path');
+
+const filesToRename = [
+  { src: 'codice.fiscale.var.js', dest: 'codice-fiscale-var.js' },
+  { src: 'codice.fiscale.commonjs2.js', dest: 'codice-fiscale-commonjs2.js' },
+  { src: 'codice.fiscale.umd.js', dest: 'codice-fiscale-umd.js' },
+  { src: 'codice.fiscale.amd.js', dest: 'codice-fiscale-amd.js' },
+];
+
+filesToRename.forEach(({ src, dest }) => {
+  const srcPath = path.join(__dirname, 'dist', src);
+  const destPath = path.join(__dirname, 'dist', dest);
+
+  if (fs.existsSync(srcPath)) {
+    fs.renameSync(srcPath, destPath);
+    console.log(`Renamed ${src} → ${dest}`);
+  } else {
+    console.warn(`File not found: ${src}`);
+  }
+});
+console.log('Renaming completed.');
